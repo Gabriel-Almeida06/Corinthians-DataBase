56b6591d9ff145ce99f2b55bcf095b45abf68a45
Gabriel <algabriel0609@gmail.com>
2026-09-24 01:00:47 -0300

    refactor: atualizado o tipo das colunas caracteristica e posicao

--- 03_insert_data.sql
+++ 03_insert_data.sql
@@ -1,3 +1,7 @@
+--Inserindo valores um por um.
 INSERT INTO elenco (camisa, nome, idade, posicao, nacionalidade, pe_dominante, caracteristica)
 VALUES
 (1, 'Hugo Souza', 27, 'Goleiro', 'Brasil', 'Direito', 'Reflexo,Agilidade,Pênalti');
+
+INSERT INTO elenco(camisa, nome, idade, posicao, nacionalidade,pe_dominante,caracteristica,peso_kg)
+VALUES (2,'mateuzinho',26,'Lateral Direito', 'Brasil', 'Direito','Velocidade,Raça,Carrinho',73 );
--- 04_alter_table.sql
+++ 04_alter_table.sql
@@ -5,3 +5,11 @@ ADD column peso_kg INT NOT NULL DEFAULT 0; --Adciona a coluna Peso
 UPDATE elenco --Atualiza o peso do unico jogador que havia sido implementado ate o momento
 SET peso_kg = 93
 WHERE id = 1;
+
+ALTER TABLE elenco 
+ALTER COLUMN posicao 
+TYPE VARCHAR(50); -- aumenta o limite de letras para 50
+
+ALTER TABLE elenco
+ALTER column caracteristica
+TYPE TEXT;  --muda o tipo para TEXT
